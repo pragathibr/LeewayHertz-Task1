29475eb2ae331fa55196df413c8920786e8edc30
Pragathi B R <43509523+pragathibr@users.noreply.github.com>
2020-08-08 14:02:27 +0530

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,49 @@
+import {combineReducers} from 'redux';
+import * as actionTypes from '../actions/types';
 
+const initialFnameState={
+    fname: 'Pragathi',
+    lname: 'Ravikumar',
+    gender: 'female',
+    email: 'pragathiravikumarbr@gmail.com',
+    password: 'abc',
+}
+
+const fname_reducer=(state= initialFnameState, action)=>{
+    switch(action.type){
+        case actionTypes.SET_FNAME:
+            return{
+                ...state,
+                fname: action.payload.fname
+            };
+        case actionTypes.SET_LNAME:
+            return{
+                ...state,
+                lname: action.payload.lname
+            };
+        case actionTypes.SET_GENDER:
+            return{
+                ...state,
+                gender: action.payload.gender
+            };
+        case actionTypes.SET_EMAIL:
+            return{
+                ...state,
+                email: action.payload.email
+            };
+        case actionTypes.SET_PASSWORD:
+            return{
+                ...state,
+                password: action.payload.password
+            };
+        default:
+            return state;
+    }
+}
+
+
+const rootReducer= combineReducers({
+    user: fname_reducer,
+});
+
+export default rootReducer;
